012ed4a7e334e90671feca9cef57d01927885002
sihyun10 <hyhy200170@gmail.com>
2025-06-25 22:28:02 +0900

    chore: 프로젝트 초기 세팅 완료

---
+++ src/test/java/com/tryiton/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tryiton.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
