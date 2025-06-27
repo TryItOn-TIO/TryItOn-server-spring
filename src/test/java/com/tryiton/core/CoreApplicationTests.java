83f68262a017823bf5714a9fa143668ed74cd22f
ahpicl64 <ahpicl64@gmail.com>
2025-06-28 04:06:57 +0900

    fix: AWS 의존성으로 인한 테스트 실패 문제 해결
    - 테스트용 application.properties 추가
    - CI/CD용 application-ci.properties 프로파일 생성
    - 테스트 클래스에서 AWS 설정 비활성화
    - build.gradle에 테스트 환경 설정 추가

--- src/test/java/com/tryiton/core/CoreApplicationTests.java
+++ src/test/java/com/tryiton/core/CoreApplicationTests.java
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {
-		"spring.config.import="
+		"spring.config.import=",
+		"spring.cloud.aws.secretsmanager.enabled=false",
+		"spring.cloud.aws.region.static=us-east-1",
+		"spring.cloud.aws.credentials.access-key=test",
+		"spring.cloud.aws.credentials.secret-key=test"
 })
 class CoreApplicationTests {
 
