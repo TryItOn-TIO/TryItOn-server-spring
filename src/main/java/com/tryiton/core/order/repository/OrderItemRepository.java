89c601733eaff1951f7250225721cc34374d2bdd
Yunkick <114406361+Yunkick@users.noreply.github.com>
2025-07-01 23:48:51 +0900

    Revert "Feat/orderpayment v2"

--- src/main/java/com/tryiton/core/order/repository/OrderItemRepository.java
+++
@@ -1,6 +0,0 @@
-package com.tryiton.core.order.repository;
-
-import com.tryiton.core.order.entity.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {}
