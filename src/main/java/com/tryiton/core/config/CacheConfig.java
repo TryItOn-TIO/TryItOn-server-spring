3641a94a7c33f78cf2f18ddcbb818ee2f4e1e1d1
ahpicl64 <ahpicl64@gmail.com>
2025-07-18 07:18:38 +0900

    fix: properties 플레이스 홀더 수정

--- src/main/java/com/tryiton/core/config/CacheConfig.java
+++ src/main/java/com/tryiton/core/config/CacheConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -17,6 +18,7 @@ import java.util.Map;
 
 @Configuration
 @EnableCaching
+@Profile("!local")
 public class CacheConfig {
 
     @Bean
--- src/main/java/com/tryiton/core/config/CacheRedisConfig.java
+++ src/main/java/com/tryiton/core/config/CacheRedisConfig.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
@@ -16,6 +17,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@Profile("!local")
 public class CacheRedisConfig {
 
     @Value("${cache.redis.host}")
