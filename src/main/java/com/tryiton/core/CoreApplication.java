c73d5919b1a58be13c8750bd61073569fa59e87e
ahpicl64 <ahpicl64@gmail.com>
2025-07-19 05:35:33 +0900

    fix: DB 커넥션 풀 고갈 문제 해결 (#109)

--- src/main/java/com/tryiton/core/CoreApplication.java
+++ src/main/java/com/tryiton/core/CoreApplication.java
@@ -2,6 +2,7 @@ package com.tryiton.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing //BaseTimeEntity
 @EnableAsync
 @EnableScheduling
+@EnableCaching
 @SpringBootApplication
 public class CoreApplication {
 
